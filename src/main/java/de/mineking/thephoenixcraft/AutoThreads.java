af971df07682e7cb8e0df66b48cf489119d46778
MineKing <mineking9534@gmail.com>
2023-06-25 16:34:12 +0200

    Improve thread naming and filter non-media messages

--- src/main/java/de/mineking/thephoenixcraft/AutoThreads.java
+++ src/main/java/de/mineking/thephoenixcraft/AutoThreads.java
@@ -1,12 +1,17 @@
 package de.mineking.thephoenixcraft;
 
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.attribute.IThreadContainer;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.regex.Pattern;
+
 public class AutoThreads extends ListenerAdapter {
+	public static final Pattern URL_PATTERN = Pattern.compile("(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]", Pattern.CASE_INSENSITIVE);
+
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event) {
 		if(event.getChannel().getIdLong() != Main.config.picvid) {
@@ -17,6 +22,29 @@ public class AutoThreads extends ListenerAdapter {
 			return;
 		}
 
-		channel.createThreadChannel(StringUtils.abbreviate(event.getMessage().getContentRaw(), ThreadChannel.MAX_NAME_LENGTH), event.getMessageIdLong()).queue();
+		if(event.getMessage().getAttachments().isEmpty() && !URL_PATTERN.asPredicate().test(event.getMessage().getContentRaw())) {
+			event.getMessage().delete().queue();
+			return;
+		}
+
+		channel.createThreadChannel(StringUtils.abbreviate(getThreadName(event.getMessage()), ThreadChannel.MAX_NAME_LENGTH), event.getMessageIdLong()).queue();
+	}
+
+	public String getThreadName(Message message) {
+		var content = message.getContentRaw().replaceAll(URL_PATTERN.pattern(), "").trim();
+
+		if(content.isEmpty()) {
+			if(!message.getEmbeds().isEmpty()) {
+				var title = message.getEmbeds().get(0).getTitle();
+
+				if(title != null) {
+					return title;
+				}
+			}
+
+			return message.getAuthor().getGlobalName();
+		}
+
+		return content;
 	}
 }
